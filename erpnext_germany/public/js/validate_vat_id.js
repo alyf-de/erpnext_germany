f56188995079baa9d484a52e66741eeb92c598a7
barredterra <14891507+barredterra@users.noreply.github.com>
2022-04-20 01:33:22 +0200

    feat(vat validation): error handling and caching

--- erpnext_germany/public/js/validate_vat_id.js
+++ erpnext_germany/public/js/validate_vat_id.js
@@ -23,23 +23,23 @@ erpnext_germany.utils.setup_vat_id_validation_button = function (frm) {
 		const vat_id = frm.doc.tax_id;
 		erpnext_germany.utils.check_vat_id(vat_id)
 			.then((is_valid) => {
-				if (is_valid) {
+				if (is_valid === true) {
 					frappe.show_alert({
 						message: __("Tax ID is a valid EU VAT ID"),
 						indicator: "green",
 					});
-				} else {
+				} else if (is_valid === false) {
 					frappe.show_alert({
 						message: __("Tax ID is not a valid EU VAT ID"),
 						indicator: "red",
 					});
+				} else {
+					frappe.show_alert({
+						message: __("Tax ID could not be validated"),
+						indicator: "grey",
+					});
 				}
-			}).catch(() => 
-				frappe.show_alert({
-					message: __("Tax ID could not be validated"),
-					indicator: "grey",
-				})
-			);
+			});
 	});
 };
 
