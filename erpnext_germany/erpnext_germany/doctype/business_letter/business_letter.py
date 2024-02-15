9f2940b7000a8094d9fde6eb2ee8f74cc883da9f
scdanieli <23150094+scdanieli@users.noreply.github.com>
2024-02-15 16:51:51 +0100

    feat: add info to referenced docs on submit and cancel
    (cherry picked from commit b1471512d4df41994f2869c2a22f174defd98375)

--- erpnext_germany/erpnext_germany/doctype/business_letter/business_letter.py
+++ erpnext_germany/erpnext_germany/doctype/business_letter/business_letter.py
@@ -2,13 +2,16 @@
 # For license information, please see license.txt
 
 import frappe
+from frappe import _
 from frappe.model.document import Document
+from frappe.desk.doctype.notification_log.notification_log import get_title
 
 
 class BusinessLetter(Document):
 	def before_validate(self):
 		self.set_subject_preview()
 		self.set_content_preview()
+		self.set_link_title()
 
 	def set_subject_preview(self):
 		self.subject_preview = (
@@ -19,6 +22,12 @@ class BusinessLetter(Document):
 		self.content_preview = (
 			frappe.render_template(self.content, self.get_context()) if self.content else " "
 		)
+
+	def set_link_title(self):
+		if self.link_name:
+			self.link_title = get_title(self.link_document_type, self.link_name)
+		else:
+			self.link_title = None
 
 	def get_context(self):
 		address = frappe.get_doc("Address", self.address) if self.address else None
@@ -32,6 +41,40 @@ class BusinessLetter(Document):
 			"contact": contact,
 			"reference": reference,
 		}
+
+	def on_submit(self):
+		self.add_submit_comments()
+
+	def on_cancel(self):
+		self.add_cancel_comments()
+
+	def add_submit_comments(self):
+		msg = _(
+			"submitted Business Letter: <a href='/app/business-letter/{0}'><strong>{1}</strong></a>"
+		).format(self.name, self.subject_preview)
+
+		if self.contact:
+			frappe.get_doc("Contact", self.contact).add_comment("Info", msg)
+
+		if self.address:
+			frappe.get_doc("Address", self.address).add_comment("Info", msg)
+
+		if self.link_name:
+			frappe.get_doc(self.link_document_type, self.link_name).add_comment("Info", msg)
+
+	def add_cancel_comments(self):
+		msg = _(
+			"canceled Business Letter: <a href='/app/business-letter/{0}'><strong>{1}</strong></a>"
+		).format(self.name, self.subject_preview)
+
+		if self.contact:
+			frappe.get_doc("Contact", self.contact).add_comment("Info", msg)
+
+		if self.address:
+			frappe.get_doc("Address", self.address).add_comment("Info", msg)
+
+		if self.link_name:
+			frappe.get_doc(self.link_document_type, self.link_name).add_comment("Info", msg)
 
 	@frappe.whitelist()
 	def get_template(self):
