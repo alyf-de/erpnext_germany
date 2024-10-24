c6b4f54a0c707a732270a86683d9a94b35b3d6fd
barredterra <14891507+barredterra@users.noreply.github.com>
2024-10-24 18:19:15 +0200

    patch: add_business_trip_to_expense_claim

--- erpnext_germany/patches/add_business_trip_to_expense_claim.py
+++ erpnext_germany/patches/add_business_trip_to_expense_claim.py
@@ -1,24 +1,20 @@
-import frappe
-from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
+from erpnext_germany.custom_fields import _
 from frappe import get_installed_apps
+from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 
-def _(message: str) -> str:
-	return message
-  
-def get_custom_fields():
-	custom_fields = {}
-    if "hrms" in get_installed_apps():
-        custom_fields["Expense Claim"] = [
-            {
-                "fieldtype": "Link",
-                "fieldname": "business_trip",
-                "label": _("Business Trip"),
-                "options": "Business Trip",
-                "insert_after": "company",
-            },
-        ]
-
-    return custom_fields
 
 def execute():
-    create_custom_fields(get_custom_fields())
+	if "hrms" in get_installed_apps():
+		create_custom_fields(
+			{
+				"Expense Claim": [
+					{
+						"fieldtype": "Link",
+						"fieldname": "business_trip",
+						"label": _("Business Trip"),
+						"options": "Business Trip",
+						"insert_after": "company",
+					},
+				]
+			}
+		)
