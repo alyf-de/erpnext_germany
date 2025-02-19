669917037ca0fc9ee8dc7c6a10b43bb2a4c0a2bd
0xD0M1M0 <76812428+0xD0M1M0@users.noreply.github.com>
2025-02-19 22:18:00 +0100

    feat: Add author, department (#49)
    * feat: Add cost center and project transfer to expense claim
    
    * feat: Add author, department, fix Jinja in content
    
    * Revert "feat: Add cost center and project transfer to expense claim"
    
    This reverts commit 323e0a45c6dab97ce7fa1c3f17afd8976bfffe40.
    
    * feat: add rights for employee to select
    
    Add employee rights to select, if not trip creation fails
    Add someone for accounting to manage
    
    * Revert "feat: add rights for employee to select"
    
    This reverts commit a79cf7cb23eaa7e3c136d6f4e938dac5baf6acd4.
    
    * fix: remove reqd, change author to sender
    
    * fix: narrow down from company to department to employee
    
    * refactor: use doc parameter in set_query, move to setup hook
    
    ---------
    
    Co-authored-by: barredterra <14891507+barredterra@users.noreply.github.com>

--- erpnext_germany/erpnext_germany/doctype/business_letter/business_letter.js
+++ erpnext_germany/erpnext_germany/doctype/business_letter/business_letter.js
@@ -2,6 +2,25 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Business Letter", {
+	setup(frm) {
+		frm.set_query("department", function (doc) {
+			return {
+				filters: {
+					company: doc.company,
+				},
+			};
+		});
+
+		frm.set_query("employee", function (doc) {
+			return {
+				filters: {
+					company: doc.company,
+					status: "Active",
+					...(doc.department && { department: doc.department }),
+				},
+			};
+		});
+	},
 	onload(frm) {
 		set_html_data(frm);
 	},
