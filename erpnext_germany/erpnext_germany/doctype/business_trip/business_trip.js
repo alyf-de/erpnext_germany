92c9c431b7ef7b51f9e088a62be88e8e9a572e26
barredterra <14891507+barredterra@users.noreply.github.com>
2025-02-20 15:06:58 +0100

    style: format with prettier

--- erpnext_germany/erpnext_germany/doctype/business_trip/business_trip.js
+++ erpnext_germany/erpnext_germany/doctype/business_trip/business_trip.js
@@ -7,8 +7,8 @@ frappe.ui.form.on("Business Trip", {
 		frm.set_query("region", (doc) => {
 			return {
 				filters: {
-					valid_from: ["<=", doc.from_date], 
-				}
+					valid_from: ["<=", doc.from_date],
+				},
 			};
 		});
 	},
@@ -44,28 +44,51 @@ frappe.ui.form.on("Business Trip Accommodation", {
 });
 
 frappe.ui.form.on("Business Trip Allowance", {
-    allowances_add(frm) {
-		if (!frm.doc.from_date || !frm.doc.to_date || frm.doc.to_date < frm.doc.from_date) {
-            frappe.msgprint(__('Please enter a correct start and end date of the trip!'));
-            return;
-        }
+	allowances_add(frm) {
+		if (
+			!frm.doc.from_date ||
+			!frm.doc.to_date ||
+			frm.doc.to_date < frm.doc.from_date
+		) {
+			frappe.msgprint(
+				__("Please enter a correct start and end date of the trip!")
+			);
+			return;
+		}
 
-        if (frm.doc.allowances && frm.doc.allowances.length == 1) {
-            let start = new Date(frm.doc.from_date);
+		if (frm.doc.allowances && frm.doc.allowances.length == 1) {
+			let start = new Date(frm.doc.from_date);
 			let end = new Date(frm.doc.to_date);
-			
+
 			frm.clear_table("allowances");
 
-			for (let d = new Date(start); d <= end; d.setDate(d.getDate() + 1)) {
+			for (
+				let d = new Date(start);
+				d <= end;
+				d.setDate(d.getDate() + 1)
+			) {
 				let child = frm.add_child("allowances");
 				let d_string = d.toISOString().slice(0, 10);
-				frappe.model.set_value(child.doctype, child.name, "date", d_string);
-				if (d_string != start.toISOString().slice(0, 10) && d_string != end.toISOString().slice(0, 10)) {
-					frappe.model.set_value(child.doctype, child.name, "whole_day", true);
+				frappe.model.set_value(
+					child.doctype,
+					child.name,
+					"date",
+					d_string
+				);
+				if (
+					d_string != start.toISOString().slice(0, 10) &&
+					d_string != end.toISOString().slice(0, 10)
+				) {
+					frappe.model.set_value(
+						child.doctype,
+						child.name,
+						"whole_day",
+						true
+					);
 				}
 			}
 
 			frm.refresh_field("allowances");
-        }
+		}
 	},
 });
