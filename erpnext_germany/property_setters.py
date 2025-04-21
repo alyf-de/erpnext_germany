a94f3ff2b0f10678984a9f192f35386a8809b1ec
barredterra <14891507+barredterra@users.noreply.github.com>
2025-04-22 00:04:36 +0200

    fix(Employee): remove property setters for "bank_ac_no" and "ctc"

--- erpnext_germany/property_setters.py
+++ erpnext_germany/property_setters.py
@@ -2,8 +2,6 @@ def get_property_setters():
 	return {
 		"Employee": [
 			("salary_currency", "default", "EUR"),
-			("bank_ac_no", "label", "IBAN"),
-			("ctc", "hidden", 1),
 			("salary_mode", "default", "Bank"),
 			("permanent_accommodation_type", "hidden", 1),
 			("current_accommodation_type", "hidden", 1),
