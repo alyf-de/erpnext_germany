a225f729492817d707adda72a5702cab8eef31e1
Raffael Meyer <14891507+barredterra@users.noreply.github.com>
2025-04-21 23:52:15 +0200

    refactor: move property setters to separate file (#73)

--- erpnext_germany/install.py
+++ erpnext_germany/install.py
@@ -3,10 +3,15 @@ from csv import DictReader
 
 import frappe
 from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
+from frappe.custom.doctype.customize_form.customize_form import (
+	docfield_properties,
+	doctype_properties,
+)
 from frappe.custom.doctype.property_setter.property_setter import make_property_setter
 from frappe.exceptions import DuplicateEntryError
 
 from .custom_fields import get_custom_fields
+from .property_setters import get_property_setters
 
 
 def after_install():
@@ -46,15 +51,27 @@ def import_csv(doctype, path):
 
 
 def make_property_setters():
-	germany_property_setters = frappe.get_hooks("germany_property_setters")
-	for doctypes, property_setters in germany_property_setters.items():
+	for doctypes, property_setters in get_property_setters().items():
 		if isinstance(doctypes, str):
 			doctypes = (doctypes,)
 
 		for doctype in doctypes:
 			for property_setter in property_setters:
-				for_doctype = not property_setter[0]
-				make_property_setter(doctype, *property_setter, for_doctype)
+				if property_setter[0]:
+					for_doctype = False
+					property_type = docfield_properties[property_setter[1]]
+				else:
+					for_doctype = True
+					property_type = doctype_properties[property_setter[1]]
+
+				make_property_setter(
+					doctype=doctype,
+					fieldname=property_setter[0],
+					property=property_setter[1],
+					value=property_setter[2],
+					property_type=property_type,
+					for_doctype=for_doctype,
+				)
 
 
 def make_custom_fields():
