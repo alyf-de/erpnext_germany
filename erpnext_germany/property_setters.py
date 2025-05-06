f047b8ec625d32f09aea578ceb4ee79b675e49bd
Raffael Meyer <14891507+barredterra@users.noreply.github.com>
2025-05-06 14:55:49 +0200

    feat: enable "Protect Attached Files" for numerous DocTypes (#75)

--- erpnext_germany/property_setters.py
+++ erpnext_germany/property_setters.py
@@ -1,3 +1,29 @@
+PROTECTED_FILE_DOCTYPES = (
+	"Quotation",
+	"Sales Order",
+	"Delivery Note",
+	"Sales Invoice",
+	"Request for Quotation",
+	"Supplier Quotation",
+	"Purchase Order",
+	"Purchase Receipt",
+	"Purchase Invoice",
+	"Journal Entry",
+	"Payment Entry",
+	"Asset",
+	"Asset Depreciation Schedule",
+	"Asset Repair",
+	"Asset Value Adjustment",
+	"Asset Capitalization",
+	"POS Invoice",
+	"Dunning",
+	"Business Letter",
+	"Period Closing Voucher",
+	"Contract",
+	"Blanket Order",
+)
+
+
 def get_property_setters():
 	return {
 		"Employee": [
@@ -5,5 +31,8 @@ def get_property_setters():
 			("salary_mode", "default", "Bank"),
 			("permanent_accommodation_type", "hidden", 1),
 			("current_accommodation_type", "hidden", 1),
+		],
+		PROTECTED_FILE_DOCTYPES: [
+			(None, "protect_attached_files", 1),
 		],
 	}
