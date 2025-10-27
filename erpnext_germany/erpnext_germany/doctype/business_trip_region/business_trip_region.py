8b0848dfe95e9b785380811d9126537ecba68c81
SathwikGuduguntla <guduguntlasathwik@gmail.com>
2025-10-27 18:00:01 +0000

    feat: adjust Business Trip logic to use new data structure

--- erpnext_germany/erpnext_germany/doctype/business_trip_region/business_trip_region.py
+++ erpnext_germany/erpnext_germany/doctype/business_trip_region/business_trip_region.py
@@ -1,24 +1,41 @@
 # Copyright (c) 2024, ALYF GmbH and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
-
+from frappe import _  
+from typing import TYPE_CHECKING 
 
 class BusinessTripRegion(Document):
-	# begin: auto-generated types
-	# This code is auto-generated. Do not modify anything in this block.
+   
+    if TYPE_CHECKING:
+       
+        from erpnext_germany.erpnext_germany.doctype.business_trip_region_allowance.business_trip_region_allowance import BusinessTripRegionAllowance
+        from frappe.types import DF
 
-	from typing import TYPE_CHECKING
+        accommodation: DF.Currency
+        allowances: DF.Table[BusinessTripRegionAllowance] 
+        arrival_or_departure: DF.Currency
+        disabled: DF.Check
+        title: DF.Data | None
+        valid_from: DF.Date | None
+        whole_day: DF.Currency
+    
+    def validate(self):
+        """
+        Validate that no child row's 'valid_till' date
+        is the same as the parent's 'valid_from' date.
+        """
+        if not self.valid_from:
+            return 
 
-	if TYPE_CHECKING:
-		from frappe.types import DF
-
-		accommodation: DF.Currency
-		arrival_or_departure: DF.Currency
-		disabled: DF.Check
-		title: DF.Data | None
-		valid_from: DF.Date | None
-		whole_day: DF.Currency
-	# end: auto-generated types
-	pass
+        for row in self.allowances:
+            if row.valid_till and row.valid_till == self.valid_from:
+                
+                frappe.throw(
+                    _("Row #{0}: The 'Valid Till' date ({1}) cannot be the same as the parent's 'Valid From' date.").format(
+                        row.idx, 
+                        row.valid_till
+                    ),
+                    title="Invalid Date"
+                )
