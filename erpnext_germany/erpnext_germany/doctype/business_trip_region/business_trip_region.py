085e015bea2c41d25b9c240a86a16b2123388c1c
SathwikGuduguntla <guduguntlasathwik@gmail.com>
2025-10-29 02:49:19 +0000

    Merge stashed changes and resolve conflicts

--- erpnext_germany/erpnext_germany/doctype/business_trip_region/business_trip_region.py
+++ erpnext_germany/erpnext_germany/doctype/business_trip_region/business_trip_region.py
@@ -1,5 +1,6 @@
 # Copyright (c) 2024, ALYF GmbH and contributors
 # For license information, please see license.txt
+<<<<<<< Updated upstream
 import frappe
 from frappe.model.document import Document
 from frappe.utils import getdate
@@ -65,3 +66,44 @@ class BusinessTripRegion(Document):
                 )
 
             seen_dates.add(vt_date)
+=======
+
+import frappe
+from frappe.model.document import Document
+from frappe import _  
+from typing import TYPE_CHECKING 
+
+class BusinessTripRegion(Document):
+   
+    if TYPE_CHECKING:
+       
+        from erpnext_germany.erpnext_germany.doctype.business_trip_region_allowance.business_trip_region_allowance import BusinessTripRegionAllowance
+        from frappe.types import DF
+
+        accommodation: DF.Currency
+        allowances: DF.Table[BusinessTripRegionAllowance] 
+        arrival_or_departure: DF.Currency
+        disabled: DF.Check
+        title: DF.Data | None
+        valid_from: DF.Date | None
+        whole_day: DF.Currency
+    
+    def validate(self):
+        """
+        Validate that no child row's 'valid_till' date
+        is the same as the parent's 'valid_from' date.
+        """
+        if not self.valid_from:
+            return 
+
+        for row in self.allowances:
+            if row.valid_till and row.valid_till == self.valid_from:
+                
+                frappe.throw(
+                    _("Row #{0}: The 'Valid Till' date ({1}) cannot be the same as the parent's 'Valid From' date.").format(
+                        row.idx, 
+                        row.valid_till
+                    ),
+                    title="Invalid Date"
+                )
+>>>>>>> Stashed changes
--- erpnext_germany/erpnext_germany/doctype/business_trip_region_allowance/business_trip_region_allowance.py
+++ erpnext_germany/erpnext_germany/doctype/business_trip_region_allowance/business_trip_region_allowance.py
@@ -1,9 +1,34 @@
 # Copyright (c) 2025, ALYF GmbH and contributors
 # For license information, please see license.txt
 
+<<<<<<< Updated upstream
 # import frappe
 # from frappe.model.document import Document
 
 
 # class BusinessTripRegionAllowance(Document):
 # 	  pass
+=======
+import frappe
+from frappe.model.document import Document
+
+
+class BusinessTripRegionAllowance(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		accommodation: DF.Currency
+		arrival__departure: DF.Currency
+		full_day: DF.Currency
+		parent: DF.Data
+		parentfield: DF.Data
+		parenttype: DF.Data
+		valid_till: DF.Date | None
+	# end: auto-generated types
+	pass
+>>>>>>> Stashed changes
