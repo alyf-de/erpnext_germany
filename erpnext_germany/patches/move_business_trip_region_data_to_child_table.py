321b4cfebabf671a3a3158fb9df30e54f76a6960
Patrick Eissler <77415730+PatrickDEissler@users.noreply.github.com>
2025-12-22 17:26:22 +0100

    fix: avoid duplicates

--- erpnext_germany/patches/move_business_trip_region_data_to_child_table.py
+++ erpnext_germany/patches/move_business_trip_region_data_to_child_table.py
@@ -4,12 +4,13 @@ import frappe
 def execute():
 	"""
 	Move the data from the old fields (on doc level) to the new child table.
+	And rename the documents.
 	"""
-	for business_trip_region_id in frappe.get_all("Business Trip Region", filters={"disabled": 0}, pluck="name"):
+	regions = frappe.get_all("Business Trip Region", filters={"disabled": 0}, pluck="name")
+	for business_trip_region_id in regions:
 		doc = frappe.get_doc("Business Trip Region", business_trip_region_id)
-		if doc.allowances:
-			continue
 
+		# Step 1: Move the data to the child table.
 		doc.append(
 			"allowances",
 			{
@@ -20,3 +21,11 @@ def execute():
 			}
 		)
 		doc.save()
+
+		# Step 2: Rename documents.
+		# Since the valid_from field based on the child table, there is no more need for several documents with the same title.
+		# Therefore, the title should be used as the new name.
+		try:
+			frappe.rename_doc("Business Trip Region", business_trip_region_id, doc.title, force=True)
+		except Exception as e:
+			frappe.log_error(e)
