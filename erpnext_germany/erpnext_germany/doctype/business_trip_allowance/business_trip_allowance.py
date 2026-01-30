579e360f91ad120e5fc890fa3b29ad83012f1540
Patrick Eißler <77415730+PatrickDEissler@users.noreply.github.com>
2026-01-30 19:08:03 +0100

    fix(Business Trip): no allowance for single-day trip with less than 8h (#114)
    Co-authored-by: barredterra <14891507+barredterra@users.noreply.github.com>

--- erpnext_germany/erpnext_germany/doctype/business_trip_allowance/business_trip_allowance.py
+++ erpnext_germany/erpnext_germany/doctype/business_trip_allowance/business_trip_allowance.py
@@ -2,7 +2,10 @@
 # For license information, please see license.txt
 
 # import frappe
+from datetime import datetime
+
 from frappe.model.document import Document
+from frappe.utils.data import get_time, getdate
 
 
 class BusinessTripAllowance(Document):
@@ -27,4 +30,19 @@ class BusinessTripAllowance(Document):
 		to_time: DF.Time | None
 		whole_day: DF.Check
 	# end: auto-generated types
-	pass
+
+	def is_longer_than(self, hours: float) -> bool:
+		if self.whole_day:
+			duration_hours = 24
+		else:
+			cur_date = getdate(self.date)
+			duration_hours = get_duration_hours(
+				from_datetime=datetime.combine(cur_date, get_time(self.from_time)),
+				to_datetime=datetime.combine(cur_date, get_time(self.to_time)),
+			)
+
+		return duration_hours > hours
+
+
+def get_duration_hours(from_datetime: datetime, to_datetime: datetime) -> float:
+	return (to_datetime - from_datetime).total_seconds() / 3600
