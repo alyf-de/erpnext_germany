ff5abce8aa954e67078d169fc1f63a344df96c33
barredterra <14891507+barredterra@users.noreply.github.com>
2026-09-17 15:11:23 +0200

    style: format with prettier

--- erpnext_germany/erpnext_germany/doctype/gdpdu_export/gdpdu_export.js
+++ erpnext_germany/erpnext_germany/doctype/gdpdu_export/gdpdu_export.js
@@ -11,9 +11,13 @@ frappe.ui.form.on("GDPdU Export", {
 			frm.add_custom_button(__("Download"), () => window.open(frm.doc.export_file));
 		} else if (frm.doc.status === "Failed") {
 			frm.dashboard.set_headline(__("The export failed, see the error log."));
-			frm.add_custom_button(__("Retry"), () => frm.call("enqueue_export").then(() => frm.reload_doc()));
+			frm.add_custom_button(__("Retry"), () =>
+				frm.call("enqueue_export").then(() => frm.reload_doc())
+			);
 		} else {
-			frm.dashboard.set_headline(__("The export is being generated. Reload this page in a moment."));
+			frm.dashboard.set_headline(
+				__("The export is being generated. Reload this page in a moment.")
+			);
 		}
 	},
 });
